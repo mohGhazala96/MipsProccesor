5ffdd5edbe7d5a99c60cc7ac98912aa04cb73ec8
Mohamed AbouGhazala <moh.abou.ghazala@gmail.com>
2018-05-02 22:54:48 +0200

    changed pc to 32 bits

--- pc.v
+++ pc.v
@@ -1,9 +1,9 @@
 module PC(addressToInstructionMemory,incrementAddress,branchAddress,PCSrc,clock);
     input clock;
     input PCSrc;
-    input [7:0] branchAddress ;
-    input [7:0] incrementAddress ;
-    output reg  [7:0] addressToInstructionMemory;
+    input [31:0] branchAddress ;
+    input [31:0] incrementAddress ;
+    output reg  [31:0] addressToInstructionMemory;
     always @(posedge clock)
         begin
             addressToInstructionMemory <= (PCSrc==1)? branchAddress: incrementAddress;
