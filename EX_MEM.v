5023092f6b9dabeac9a0991ba1dac27418a7a3ed
MustafaGoudah <Mustafagoudag@gmail.com>
2018-05-02 21:44:53 +0200

    PC modified to 32 bits in EX_MEM

--- EX_MEM.v
+++ EX_MEM.v
@@ -1,7 +1,7 @@
 module EX_MEM (WB_output,M_output, PC_output ,zero_output, ALUresult_output,writeData_output,writeRegister_output,WB,M,PC,zero,writeData,ALUresult,writeRegister,clk);
 input [1:0] WB; 
 input [2:0] M;
-input [7:0] PC;
+input [31:0] PC;
 input [31:0] ALUresult,writeData;
 input zero;
 input [4:0] writeRegister;
@@ -10,7 +10,7 @@ input clk;
 output reg [1:0] WB_output;
 output reg [31:0] ALUresult_output,writeData_output;
 output reg zero_output;
-output reg [7:0] PC_output;
+output reg [31:0] PC_output;
 output reg [4:0] writeRegister_output;
 output reg [2:0] M_output;
 always @(posedge clk)
