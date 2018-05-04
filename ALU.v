03033be1e4753ac96777681d4a977cafd40626ba
Amjad Mashaal <me@amjad.io>
2018-05-04 16:11:03 +0200

    Adding shifting to ALU

--- ALU.v
+++ ALU.v
@@ -1,6 +1,7 @@
-module ALU(result, zero, in1, in2, op);
+module ALU(result, zero, in1, in2, shamt, op);
     input[31:0] in1;
     input[31:0] in2;
+    input[4:0] shamt;
     input[3:0] op;
 
     output reg zero;
@@ -13,7 +14,8 @@ module ALU(result, zero, in1, in2, op);
             4'b0010: result = in1 + in2;
             4'b0110: result = in1 - in2;
             4'b0111: result = in1 < in2 ? 1 : 0;
-            4'b1100: result = !(in1 | in2);
+            4'b1000: result = in1 << shamt;
+            4'b1001: result = in1 >> shamt;
             default: result = 0;
         endcase
 
@@ -37,6 +39,8 @@ module ALUControl(out, func, op);
                        6'b100100: out = 4'b0000;
                        6'b100101: out = 4'b0001;
                        6'b101010: out = 4'b0111;
+                       6'b000000: out = 4'b1000;
+                       6'b000010: out = 4'b1001;
                        default: out = 4'bzzzz;
                    endcase
             default: out = 4'bzzzz;
@@ -46,19 +50,21 @@ endmodule
 module TestALU;
     reg[5:0] func;
     reg[3:0] op;
+    reg[4:0] shamt;
     reg[31:0] in1;
     reg[31:0] in2;
 
     wire[31:0] res;
     wire zero;
 
-    ALU alucontrol(res, zero, in1, in2, op);
+    ALU alucontrol(res, zero, in1, in2, shamt, op);
 
     initial begin
-        op = 4'b1100;
-        func = 6'b101010;
+        op = 4'b1000;
+        func = 6'b000000;
         in1 = 2'b11;
         in2 = 2'b11;
+        shamt = 5'b00010;
     end
 
     always@(res) begin
